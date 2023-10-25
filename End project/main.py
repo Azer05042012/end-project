25e2281cdec613af43296d6c9f82c0d75e5aa8cd
AZER <129597661+Azer05042012@users.noreply.github.com>
2023-10-25 20:28:34 +0400

    Update main.py

--- End project/main.py
+++ End project/main.py
@@ -4,13 +4,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
-
 #Первая страница
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
+@app.route('/resources')
+def resources():
+    return render_template('resources.html')
 
 app.run(debug=True)
